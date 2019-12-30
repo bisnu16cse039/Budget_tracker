069dad8f99bf1e1ab76e45dae14292bd2fd1bf89
bisnu16cse039 <34216949+bisnu16cse039@users.noreply.github.com>
2019-12-31 00:29:48 +0600

    Add files via upload

---
+++ project/table.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/capture-117x114.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <title>Home</title>
+  <link rel="stylesheet" href="assets/web/assets/icons/bticons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/bt/css/bt-additional.css"><link rel="stylesheet" href="assets/bt/css/bt-additional.css" type="text/css">
+  <link rel="stylesheet" href="assets/web/assets/icons/bticons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="css/xyz.css">
+  <link rel="preload" as="style" href="assets/bt/css/bt-additional.css"><link rel="stylesheet" href="assets/bt/css/bt-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+
+
+  <section class="menu cid-rI77XXmEYL" once="menu" id="menu1-5">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="http://idex.html">
+                         <img src="assets/images/capture-117x114.png" alt="bT" title="" style="height: 3.8rem;">
+                    </a>
+                </span>
+                
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+                    
+                        </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="index.php">Home&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</a>
+                </li>
+                <li class="nav-item"><a class="nav-link link text-white display-4" href="page4.php">About Us &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a></li>
+                <li class="nav-item">
+                    <div class="dropdown">
+                    <button class="dropbtn">Budget Field</button>
+                    <div class="dropdown-content">
+                        <a href="#">Programming club</a>
+                        <a href="#">Mechatronics club </a>
+                        <a href="#">Apps. club</a>
+                        <a href="#">Games club</a>
+                    </div>
+                    </div> 
+
+                    &nbsp; &nbsp;&nbsp; &nbsp;
+                </li>
+                <li class="nav-item"></a></li>
+                <li class="nav-item">
+                <a class="nav-link link text-white display-4" href="page2.html">Administrator &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a></li>
+                <li class="nav-item"></a></li>
+
+            
+        </div>
+    </nav>
+</section>
+
+
+<section class="counters1 counters cid-rI7pRyVqsV" id="counters1-l">
+
+
+
+
+
+
+
+<div class="container">
+  <h2>Table</h2>
+  <p></p>            
+  <table class="table table-bordered">
+    <thead class="thead-dark">
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+        <th>Phone</th>
+		
+		
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Bisnu</td>
+        <td>Sarkar</td>
+        <td>bisnusarkar@gmail.com</td>
+        <td>01558932394</td>
+      </tr>
+      <tr>
+        <td>Marajul</td>
+        <td>Islam</td>
+        <td>marajul@gmail.com</td>
+        <td>01791869505</td>
+      </tr>
+      <tr>
+        <td>Samim</td>
+        <td>Ferdous</td>
+        <td>realsamim@gmail.com</td>
+        <td>01755967577</td>
+      </tr>
+       <tr>
+        <td>Kazi</td>
+        <td>shawon</td>
+        <td>kazishawon23@gmail.com</td>
+        <td>0175596746647</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</section>
+
+</body>
+
+
+<section class="cid-rI7pVL2mBt" id="footer1-m">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row content text-white">
+            
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Address
+                </h5>
+                <p class="mbr-text">
+                    Gobra city,Gopalganj 8100
+                    <br>City,Gobra
+                </p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Contacts
+                </h5>
+                <p class="mbr-text">
+                    Email: support@btbsmrstu.com
+                    <br>Phone: +8801700000000000
+                    <br>Fax: +9832682572562362
+                </p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Links
+                </h5>
+                <p class="mbr-text">
+                    <a class="text-primary" href="#">Download Our Mobile App</a>
+                    <br><a class="text-primary" href="#">Download for Windows</a>
+                    <br><a class="text-primary" href="#">Download for Mac</a>
+                </p>
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row">
+                <div class="col-sm-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="media-container-row mbr-white">
+                <div class="col-sm-6 copyright">
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; © Copyright 2019 bTbsmrstu - All Rights Reserved
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <div class="social-list align-right">
+                        <div class="soc-item">
+                            <a href="https://twitter.com/#" target="_blank">
+                                <span class="socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.facebook.com/pages/#" target="_blank">
+                                <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.youtube.com/c/#" target="_blank">
+                                <span class="socicon-youtube socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://instagram.com/#" target="_blank">
+                                <span class="socicon-instagram socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://plus.google.com/u/0/#" target="_blank">
+                                <span class="socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.behance.net/#" target="_blank">
+                                <span class="socicon-behance socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+</html>
